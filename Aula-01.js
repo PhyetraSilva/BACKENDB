eb1d011872b5e3aec3980f941d9ca050788931f3
PhyetraSilva <phyetrajhamilly@gmail.com>
2025-04-03 16:26:34 -0400

    variaveis e tipos de dados

---
+++ Aula-01.js
@@ -0,0 +1,7 @@
+let valor = 13.141416;
+let text = "Meu primeiro porjeto em JavaScript";
+let ativo = true;
+
+console.log(valor.toFixed(2)); //Arredonda o valor para duas casas decimais
+console.log(text.toLocaleUpperCase()); //Converte o texto para maiúsculas
+console.log(ativo.toString()); //Converte para texto
