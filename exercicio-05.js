b35f8093c8d5034f6dbff07d1c2e2a4aaa1fb06c
PhyetraSilva <phyetrajhamilly@gmail.com>
2025-04-10 17:25:07 -0400

    aula-02

---
+++ exercicio-05.js
@@ -0,0 +1,6 @@
+let variavel1 = 20;
+let variavel2 = 2;
+console.log(variavel1 +variavel2);
+console.log(variavel1-variavel2);
+console.log(variavel1 * variavel2);
+console.log(variavel1 / variavel2);
---
+++ exercicio-06.js
@@ -0,0 +1,3 @@
+let idade1 = 10;
+let idade2 = 20;
+console.log(idade1 >= idade2 ? "Primeira pessoa mais velha" : "Segunda pessoa mais velha");
