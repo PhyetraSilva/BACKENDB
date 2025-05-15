6268287930ec3d81103d2945e8f0c1acdd2d122f
PhyetraSilva <phyetrajhamilly@gmail.com>
2025-05-15 17:35:04 -0400

    aula-03

---
+++ Atividadesobjetos.js
@@ -0,0 +1,52 @@
+//01
+let ap = {
+    quartos: 2,
+    tipo: 'Apartamento',
+    endereco: 'Avenida Principal, 456 -Centro',
+    andar: 7,
+}
+
+console.log(ap);
+console.log('${ap.tipo} com ${ap.quartos} quartos,localizado no ${ap.andar}º andar da ${ap.endereco}');
+
+//02
+let prodEmb = {
+    nome: 'Laptop HP',
+    categoria: 'Eletrônicos',
+    peso: 1.5,
+    preco: 3500.00,
+}
+console.log(`O produto${prodEmb.nome}, da categoria ${prodEmb.categoria}, pesando ${prodEmb.peso}, Está a venda por R$${prodEmb.preco.toLocaleString('pt-BR',{style: 'currency', currency:'BRL'})}.`)
+
+//03
+class Imovel {
+    constructor(quartos,tipo,endereco){
+    this.quartos = quartos;
+    this.tipo = tipo;
+    this.endereco = endereco;
+    }
+    exibirinformacoes(){
+        return `${this.tipo} com ${this.quartos} quartos,localizado na ${this.endereco}.`
+    }
+}
+
+let casa = new Imovel();
+//casa.quartos = 3;
+//casa.tipo = 'Sobrando';
+//casa.endereco = 'Rua tanto faz, nº 7070';
+//console.log(casa);
+//console.log(`Tipo do imóvel: ${casa.tipo} \nQuartos: ${casa.quartos} \nEndereço: ${casa.endereco}`);
+
+console.log(casa.exibirinformacoes());
+
+let apt = new Imovel(2,'Apartamento', 'Rua Amélia, nº 1010, bairro centro');
+console.log(apt.exibirinformacoes());
+
+//04
+
+import EntradaDados from 'readline-sync';
+
+console.log();
+let nome = entradaDados.question("Informe seu nome:");
+console.log(`Olá ${nome}, seja bem-vindo(a)!`);
+
